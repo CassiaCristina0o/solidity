d758903f341ebce0cfa656d0ea048b4b1a3e381f
cassia cristina <c.cassiacristina21@gmail.com>
2023-07-10 11:39:46 -0300

    ola

---
+++ solidity/teste2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.7;
+
+contract variavelDeEstado {
+
+    int numero;
+
+function pegaNumero() public view returns (int){
+    return numero;
+}
+function alteraNumero() public {
+    numero = 10;
+}
+
+}
